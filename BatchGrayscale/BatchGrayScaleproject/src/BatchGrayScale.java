bc6760384737859ac13726fbe66d427b8e28cea6
JOY_RU <joycseru@gmail.com>
2017-11-12 20:33:24 +0600

    start work with batchGrayScale class

---
+++ BatchGrayscale/BatchGrayScaleproject/src/BatchGrayScale.java
@@ -0,0 +1,13 @@
+import edu.duke.*;
+import java.io.*;
+public class BatchGrayScale {
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        
+		DirectoryResource dr = new DirectoryResource() ; 
+	}
+
+}
